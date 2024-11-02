afd71001c600e4d43edbac480ff2e4504a5a962e
@Slava-Fly <school.94@bk.ru>
2024-11-02 12:00:27 +0700

    New tracks

--- AudioTrackPlayer/Storage/TracklistStorage.swift
+++ AudioTrackPlayer/Storage/TracklistStorage.swift
@@ -18,8 +18,12 @@ final class TracklistStorage {
         return [
             Track(title: "No Roots", artist: "Alice Merton", duration: 149.58, fileName: "NoRoots"),
             Track(title: "Warriors", artist: "Imagine Dragons", duration: 170.11, fileName: "Warriors"),
+            Track(title: "Wandering Jew", artist: "Oxxxymiron", duration: 152.92, fileName: "WanderingJew"),
             Track(title: "Before winter", artist: "Oxxxymiron", duration: 152.30, fileName: "BeforeWinter"),
-            Track(title: "American boy", artist: "Kombination", duration: 270.24, fileName: "American-boy")
+            Track(title: "Kaifograd", artist: "Slava KPSS", duration: 153.62, fileName: "KaifoGrad"),
+            Track(title: "Nocnaia Travma", artist: "Zamai & Slava KPSS", duration: 238.26, fileName: "NocnaiaTravma"),
+            Track(title: "American boy", artist: "Kombination", duration: 270.24, fileName: "American-boy"),
+            Track(title: "Aborigen", artist: "Zamai", duration: 104.62, fileName: "Aborigen")
         ]
     }
 }
